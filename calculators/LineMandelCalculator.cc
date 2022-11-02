8c869f081361774f3d2a90021a462130fc59e6dc
DavSladk <davsladk+github@gmail.com>
2022-11-02 22:31:26 +0100

    Line implementation

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -15,97 +15,61 @@
 
 #include "LineMandelCalculator.h"
 
-//#define REDUCE
 
 LineMandelCalculator::LineMandelCalculator (unsigned matrixBaseSize, unsigned limit) :
 	BaseMandelCalculator(matrixBaseSize, limit, "LineMandelCalculator")
 {
-	// @TODO allocate & prefill memory
-	data = (int *)(aligned_alloc(ALIGMENT, height * width * sizeof(int)));
-	real = (float *)(aligned_alloc(ALIGMENT, height * width * sizeof(float)));
-	img = (float *)(aligned_alloc(ALIGMENT, height * width * sizeof(float)));
+	data = (int *)_mm_malloc(height * width * sizeof(int), ALIGMENT);
+	real = (float *)_mm_malloc(height * width * sizeof(int), ALIGMENT);
+	img = (float *)_mm_malloc(height * width * sizeof(int), ALIGMENT);
 	initData();
-
 }
 
 LineMandelCalculator::~LineMandelCalculator() {
-	// @TODO cleanup the memory
-	free(data);
-	free(real);
-	free(img);
+	_mm_free(data);
+	_mm_free(real);
+	_mm_free(img);
 	data = NULL;
 	real = NULL;
 	img = NULL;
 }
 
 int * LineMandelCalculator::calculateMandelbrot () {
-	// @TODO implement the calculator & return array of integers
-	int* pdata = data;
-	float* preal = real;
-	float* pimg = img;
-	
-	
-	for(int i = 0; i < height / 2 + 1; i++)
+	for(int i = 0; i < height / 2 ; i++)
 	{
-		#ifndef REDUCE
 		float y = y_start + i * dy; // current imaginary value
-
+		int doneCount = 0;
 		for(int l = 0; l < limit; l++)
 		{
-			#pragma omp simd aligned(pdata, preal, pimg: 64)
+			#pragma omp simd reduction(+:doneCount)//aligned(data, real, img: 64)
 			for(int j = 0; j < width; j++)
 			{
 				float x = x_start + j * dx; // current real value
 
-				int currentIndex = i * width + j;
-				float r2 = preal[currentIndex] * preal[currentIndex];
-				float i2 = pimg[currentIndex] * pimg[currentIndex];
-				
-				if(r2 + i2 > 4.0f && pdata[currentIndex] == limit)
-				{
-					pdata[currentIndex] = l;
-				}
-				else if(r2 + i2 < 4.0f)
-				{
-					pimg[currentIndex] = 2.0f * preal[currentIndex] * pimg[currentIndex] + y;
-					preal[currentIndex] = r2 - i2 + x;
-				}
+				int index = i * width + j;
 
+				float r2 = real[index] * real[index];
+				float i2 = img[index] * img[index];
+
+				if(r2+i2 > 4)
+				{
+					data[index] = std::min(data[index], l);
+				}
+				else
+				{
+					img[index] = 2.0f * real[index] * img[index] + y;
+					real[index] = r2 - i2 + x;
+				}
 			}
 		}
-		#else
-		int doneCount = 0;
-		float y = y_start + i * dy; // current imaginary value
-
-		for(int l = 0; (l < limit) && (doneCount < limit); l++)
-		{
-			#pragma omp simd aligned(pdata, preal, pimg: 64) reduction(+:doneCount)
-			for(int j = 0; j < width; j++)
-			{
-				float x = x_start + j * dx; // current real value
-
-				int currentIndex = i * width + j;
-				float r2 = preal[currentIndex] * preal[currentIndex];
-				float i2 = pimg[currentIndex] * pimg[currentIndex];
-				
-				if(r2 + i2 > 4.0f && pdata[currentIndex] == limit)
-				{
-					pdata[currentIndex] = l;
-					doneCount += 1;
-				}
-				else if(r2 + i2 < 4.0f)
-				{
-					pimg[currentIndex] = 2.0f * preal[currentIndex] * pimg[currentIndex] + y;
-					preal[currentIndex] = r2 - i2 + x;
-					doneCount += 0;
-				}
-
-			}
-		}
-		#endif
-
-		memcpy(data+(width)*(height)-i*(width), data+i*(width), width * sizeof(int));
+		//memcpy(data+(width)*(height-1)-i*(width), data+i*(width), width * sizeof(int));
 	}
+	// memcpy is outside of main loop, because compiler doesn't like it inside
+	for(int i = 0; i < height / 2 ; i++)
+	{
+		memcpy(data+(width)*(height-1)-i*(width), data+i*(width), width * sizeof(int));
+	}
+
 	return data;
 }
 
